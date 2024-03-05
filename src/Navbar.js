3e294adb2943811d290e3d5f1cab820712ce5695
lastcarol <lastcarol0315@gmail.com>
2024-03-05 21:16:25 +0900

    크로아 목업

---
+++ src/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Navbar.css";
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="qHeader_logo">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Quora_logo_2015.svg/2880px-Quora_logo_2015.svg.png"
+          alt=""
+        />
+      </div>
+      <div className="qHeader_icons">
+        <div className="qHeader_icon"></div>
+        <div className="qHeader_icon"></div>
+        <div className="qHeader_icon"></div>
+        <div className="qHeader_icon"></div>
+        <div className="qHeader_icon"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
